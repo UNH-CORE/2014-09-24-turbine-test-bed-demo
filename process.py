8f38e0d6055b95367c1349b361f5aec08223b315
Pete Bachant <petebachant@gmail.com>
2015-09-21 15:46:18 -0400

    Add top-level script for processing data

---
+++ process.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+This script runs processing functions from `Modules.processing`.
+"""
+
+from __future__ import print_function
+from Modules.processing import *
+
+
+if __name__ == "__main__":
+    print("Processing all data")
+    batch_process_all()
