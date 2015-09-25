ecc155f462a1425f0dae0095478fad03f8641ab1
Pete Bachant <petebachant@gmail.com>
2015-09-24 23:56:57 -0400

    Update process.py to run on one processor

--- process.py
+++ process.py
@@ -4,9 +4,9 @@ This script runs processing functions from `Modules.processing`.
 """
 
 from __future__ import print_function
-from Modules.processing import *
+import Modules.processing as pr
 
 
 if __name__ == "__main__":
     print("Processing all data")
-    batch_process_all()
+    pr.batch_process_all(nproc=1)
